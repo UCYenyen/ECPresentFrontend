e72269f21f61fac36044114bf6833ce8dd257de9
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-12-15 08:27:51 +0700

    changed-base-api-url

--- app/src/main/java/com/example/ecpresent/data/container/ServerContainer.kt
+++ app/src/main/java/com/example/ecpresent/data/container/ServerContainer.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class ServerContainer {
     companion object {
-        const val BASE_URL = "http://10.0.2.2:3000/api/"
+        const val BASE_URL = "http://10.0.182.156/api/"
     }
 
     private val retrofit = Retrofit.Builder()
