9d5b272e50e03544c192713fdeee42b05fe33c32
Hafizd Dharma Saputra Hardiono <hdharmasaputra12@gmail.com>
2025-12-16 20:15:09 +0700

    fix_answer_model

--- app/src/main/java/com/example/ecpresent/ui/model/Answer.kt
+++ app/src/main/java/com/example/ecpresent/ui/model/Answer.kt
@@ -1,11 +1,18 @@
 package com.example.ecpresent.ui.model
-
+import com.example.ecpresent.data.dto.Answer as AnswerDto
 data class Answer  (
     val id: Int,
-    val question: Question,
-    val audioUrl: String?,
-    val score: String?,
-    val suggestion: String?,
-    val createdAt: String,
-    val updatedAt: String
+    val score: Int,
+    val suggestion: String,
+    val audioUrl: String,
+
 )
+
+fun AnswerDto.toUiModel(): Answer {
+    return Answer(
+        id = this.id,
+        score = this.score?.toInt() ?: 0,
+        suggestion = this.suggestion ?: "No suggestion available.",
+        audioUrl = this.audioUrl ?: ""
+    )
+}
