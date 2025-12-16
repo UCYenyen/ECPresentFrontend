0f09393b362e32d0fafd6bc81eda74a02e9bebce
Hafizd Dharma Saputra Hardiono <hdharmasaputra12@gmail.com>
2025-12-16 20:15:28 +0700

    fix_feedback_model

--- app/src/main/java/com/example/ecpresent/ui/model/Feedback.kt
+++ app/src/main/java/com/example/ecpresent/ui/model/Feedback.kt
@@ -1,8 +1,10 @@
 package com.example.ecpresent.ui.model
 
+import com.example.ecpresent.enum.GradeEnum
+import com.example.ecpresent.data.dto.Feedback as FeedbackDto
 data class Feedback (
     val id: Int,
-    val presentation: Presentation,
+    val presentationId: Int,
     val expression: Float,
     val intonation: Float,
     val posture: Float,
@@ -11,17 +13,29 @@ data class Feedback (
     val overallRating: Float,
     val grade: GradeEnum,
     val videoSuggestion: String,
-    val audioSuggestion: String?,
-    val createdAt: String,
-    val updatedAt: String
+    val audioSuggestion: String
 )
 
-enum class GradeEnum {
-    A,
-    B,
-    C,
-    D,
-    E,
-    F,
-    T
+fun FeedbackDto.toUiModel(): Feedback {
+    return Feedback(
+        id = this.id,
+        presentationId = this.presentationId,
+        expression = this.expression,
+        intonation = this.intonation,
+        posture = this.posture,
+        videoScore = this.videoScore,
+
+        audioScore = this.audioScore ?: 0f,
+        overallRating = this.overallRating ?: 0f,
+
+        grade = try {
+            if (this.grade != null) GradeEnum.valueOf(this.grade) else GradeEnum.E
+        } catch (e: IllegalArgumentException) {
+            GradeEnum.UNKNOWN
+        },
+
+        videoSuggestion = this.videoSuggestion,
+        audioSuggestion = this.audioSuggestion ?: "No audio feedback available yet."
+    )
 }
+
