8c7b475ef5ca362d0edc9b3bf92d4c41630a1d39
Hafizd Dharma Saputra Hardiono <hdharmasaputra12@gmail.com>
2025-12-16 20:15:50 +0700

    fix_feedback_dto

--- app/src/main/java/com/example/ecpresent/data/dto/Feedback.kt
+++ app/src/main/java/com/example/ecpresent/data/dto/Feedback.kt
@@ -1,17 +1,30 @@
 package com.example.ecpresent.data.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class Feedback(
-    val audio_score: Int,
-    val audio_suggestion: String,
-    val createdAt: String,
-    val expression: Int,
-    val grade: String,
     val id: Int,
-    val intonation: Int,
-    val overall_rating: Int,
-    val posture: Int,
-    val presentation_id: Int,
+    @SerializedName("presentation_id")
+    val presentationId: Int,
+
+    val expression: Float,
+    val intonation: Float,
+    val posture: Float,
+
+    @SerializedName("video_score")
+    val videoScore: Float,
+    @SerializedName("audio_score")
+    val audioScore: Float?,
+    @SerializedName("overall_rating")
+    val overallRating: Float?,
+    val grade: String?,
+
+    @SerializedName("video_suggestion")
+    val videoSuggestion: String,
+    @SerializedName("audio_suggestion")
+    val audioSuggestion: String?,
+
+    val createdAt: String,
     val updatedAt: String,
-    val video_score: Double,
-    val video_suggestion: String
+
 )
