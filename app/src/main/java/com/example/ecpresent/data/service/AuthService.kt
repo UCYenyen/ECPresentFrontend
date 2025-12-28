b2292819b4119eabe2087f0b291126826876a362
Hafizd Dharma Saputra Hardiono <hdharmasaputra12@gmail.com>
2025-12-29 04:44:50 +0700

    add_endpoint_for_fun_display_profile

--- app/src/main/java/com/example/ecpresent/data/service/AuthService.kt
+++ app/src/main/java/com/example/ecpresent/data/service/AuthService.kt
@@ -1,13 +1,13 @@
 package com.example.ecpresent.data.service
 
 import com.example.ecpresent.data.dto.BaseResponse
-import com.example.ecpresent.data.dto.LearningResponseItem
 import com.example.ecpresent.data.dto.LoginUserRequest
 import com.example.ecpresent.data.dto.RegisterUserRequest
 import com.example.ecpresent.data.dto.UserResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface AuthService {
@@ -15,10 +15,17 @@ interface AuthService {
     suspend fun register(
         @Body request: RegisterUserRequest
     ): Response<BaseResponse<UserResponse>>
+
     @POST("login")
     suspend fun login(
         @Body request: LoginUserRequest
     ): Response<BaseResponse<UserResponse>>
+
     @POST("guest")
     suspend fun continueAsGuest(): Response<BaseResponse<UserResponse>>
+
+    @GET("get-profile")
+    suspend fun getProfileById(
+        @Header("Authorization") token: String
+    ): Response<BaseResponse<UserResponse>>
 }
