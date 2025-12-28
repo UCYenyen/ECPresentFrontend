8dc534a3ad48914d2e5129a470646da780148e61
Hafizd Dharma Saputra Hardiono <hdharmasaputra12@gmail.com>
2025-12-29 04:56:33 +0700

    fix_avatar_dto_to_model

--- app/src/main/java/com/example/ecpresent/ui/model/Avatar.kt
+++ app/src/main/java/com/example/ecpresent/ui/model/Avatar.kt
@@ -1,5 +1,5 @@
 package com.example.ecpresent.ui.model
-
+import com.example.ecpresent.data.Env.ROOT_URL
 import com.example.ecpresent.data.dto.AvatarResponse
 
 data class Avatar(
@@ -8,5 +8,18 @@ data class Avatar(
     val createdAt: String,
     val updatedAt: String,
 )
+
+fun AvatarResponse.toAvatar(): Avatar{
+    val rawUrl = this.imageUrl.ifBlank {
+        "uploads/avatar_2.jpg"
+    }
+    val finalUrl = "$ROOT_URL$rawUrl"
+    return Avatar(
+        id = this.id.toString(),
+        imageUrl = finalUrl,
+        createdAt = this.createdAt,
+        updatedAt = this.updatedAt
+    )
+}
 
 
