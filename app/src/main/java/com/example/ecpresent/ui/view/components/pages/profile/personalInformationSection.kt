7a4831c8df6d15e1b7ac628b494bf21b3e4a5025
Hafizd Dharma Saputra Hardiono <hdharmasaputra12@gmail.com>
2025-12-29 04:58:06 +0700

    connect_profile_backend_to_front

--- app/src/main/java/com/example/ecpresent/ui/view/components/pages/profile/personalInformationSection.kt
+++ app/src/main/java/com/example/ecpresent/ui/view/components/pages/profile/personalInformationSection.kt
@@ -14,25 +14,34 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage // Pastikan library Coil sudah di-sync
+import coil.compose.AsyncImage
+import com.example.ecpresent.R
 import com.example.ecpresent.ui.theme.ECPresentTheme
+import com.example.ecpresent.ui.uistates.ProfileUIState
+import com.example.ecpresent.ui.viewmodel.AuthViewModel
 
 @Composable
 fun PersonalInformationSection(
-    username: String = "Loading...",
-    email: String = "Loading...",
-    avatarUrl: String = ""
+    authViewModel: AuthViewModel
 ) {
+    val profileState by authViewModel.profileUIState.collectAsState()
+    val profile = if (profileState is ProfileUIState.Success){
+        (profileState as ProfileUIState.Success).data
+    } else null
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -40,20 +49,20 @@ fun PersonalInformationSection(
         verticalArrangement = Arrangement.spacedBy(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-        // 1. Bagian Avatar (Menggunakan Coil untuk URL)
         AsyncImage(
-            model = avatarUrl,
+            model = profile?.avatar?.imageUrl,
             contentDescription = "User Avatar",
             modifier = Modifier
-                .size(100.dp) // Ukuran Avatar
+                .size(100.dp)
                 .clip(CircleShape)
-                .border(2.dp, Color.Gray, CircleShape), // List tipis biar rapi
-            contentScale = ContentScale.Crop
+                .border(2.dp, Color.Gray, CircleShape),
+            contentScale = ContentScale.Crop,
+            placeholder = painterResource(R.drawable.ic_launcher_foreground),
+            error = painterResource(R.drawable.ic_launcher_background)
         )
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        // 2. Form Name (Desain SAMA PERSIS dengan kode Abang)
         Column(
             modifier = Modifier.padding(vertical = 8.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
@@ -65,8 +74,8 @@ fun PersonalInformationSection(
                 color = Color.Black
             )
             TextField(
-                value = username, // Data dari Backend masuk sini
-                onValueChange = {}, // Read Only
+                value = profile?.username ?: "Not Rendered",
+                onValueChange = {},
                 readOnly = true,
                 colors = TextFieldDefaults.colors(
                     unfocusedContainerColor = Color.LightGray,
@@ -75,7 +84,7 @@ fun PersonalInformationSection(
                     focusedIndicatorColor = Color.Transparent,
                     errorIndicatorColor = Color.Transparent,
                     focusedPlaceholderColor = Color.DarkGray,
-                    focusedContainerColor = Color.LightGray // Tambahan biar warnanya konsisten pas diklik
+                    focusedContainerColor = Color.LightGray
                 ),
                 modifier = Modifier.fillMaxWidth(),
                 shape = RoundedCornerShape(12.dp),
@@ -86,7 +95,6 @@ fun PersonalInformationSection(
             )
         }
 
-        // 3. Form Email (Desain SAMA PERSIS dengan kode Abang)
         Column(
             modifier = Modifier.padding(vertical = 8.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
@@ -98,8 +106,8 @@ fun PersonalInformationSection(
                 color = Color.Black
             )
             TextField(
-                value = email, // Data dari Backend masuk sini
-                onValueChange = {}, // Read Only
+                value = profile?.email?: "Not Rendered",
+                onValueChange = {},
                 readOnly = true,
                 colors = TextFieldDefaults.colors(
                     unfocusedContainerColor = Color.LightGray,
@@ -124,11 +132,5 @@ fun PersonalInformationSection(
 @Preview(showBackground = true)
 @Composable
 private fun PersonalInformationSectionPreview() {
-    ECPresentTheme {
-        PersonalInformationSection(
-            username = "Bryan Fernando Dinata",
-            email = "bryanfernandodinata@gmail.com",
-            avatarUrl = "https://res.cloudinary.com/demo/image/upload/sample.jpg"
-        )
-    }
+    ECPresentTheme {}
 }
